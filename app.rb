3b3ffba2561be1a9c358f42f9276782b9f3e4ae7
k-nasa <htilcs1115@gmail.com>
2019-03-16 19:35:27 +0900

    feat: line botオウム返し

---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'line/bot'
+
+class App < Sinatra::Base
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = "7251b6d94ddd87db824fc02275042c12"
+      config.channel_token = "t3pWIjQC1Hj3u6IFxIW0ocUHmoUafFP9hGUYP0ksNBQPW4zrnkccjCb95+CYiicD7ZUjjsovWoi0KbLt/aZ8JeqvlbKMGbN2auCZJ2JnVvL7QowXCMcWuGT3uUknz0vTVG+5Br0KR7Kq5AD22l0nrQdB04t89/1O/w1cDnyilFU="
+    }
+  end
+
+  get '/' do
+    "サーバー動いてるお！"
+  end
+
+  post '/callback' do
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+
+    "OK"
+  end
+end
